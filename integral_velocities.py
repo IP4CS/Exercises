2f0d45096b5c293a91af3620fe8915fc82c6fa6b
yingtang <you@example.com>
2014-09-23 23:24:56 -0400

    integral
    Integrate velocity over time according to the given data sets
    the data is called velocities.txt in the data folder.
    the result I got is: 8.137549

---
+++ integral_velocities.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pylab as pl
+
+data = np.loadtxt('velocities.txt', float) #make sure the data type is floating point
+
+#get the row and column numbers
+row, col = data.shape
+
+N=row  #N is the number of data points
+a = data[0,0] #get the first element in the x axis
+b = data[-1,0] #get the last element in the x axis
+
+spacing = (b - a)/float(N)
+
+S = 0.5*(data[0,1]+data[-1,1]) #0.5*(f(a)+f(b))
+
+for counter in range(1, N-1): #the first and last points have alread been considered
+   
+    S += data[counter,1] #sum(f(a+kh)), f(a+kh) is given in the data already
+
+S *= spacing
+
+print "The integral equals: %f"  %S
+print "Number of steps: %d"  %N 
+
+#to check what's the data look like
+pl.plot(data[:,0], data[:,1], marker='o')
+pl.xlabel('t')
+pl.ylabel('Vx')
+pl.show()
